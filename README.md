9c6281dae15a2c13c8741a1e7e28a7bd8817808d
nikossvnk <seev0@yahoo.com>
2015-03-11 22:23:19 +0200

    multiple servers support

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # fuzzy-batman
-Python script that checks if another server is online and informs you by sending an sms through nexmo. (Nexmo is a paid service)
+Python script that checks if another server(s) is/are online and informs you by sending an sms through nexmo. (Nexmo is a paid service)
+
+Why fuzzy-batman?
+Because github suggested that this would be a cool fancy name
--- check_online.py
+++ check_online.py
@@ -3,12 +3,31 @@
 
 import httplib, urllib
 
-def nexmo( message ):    
+servers = {}
+servers['s1'] = {
+            'server' : 'DOMAIN1.COM', \
+            'path' : '/',\
+            'port' : 443, \
+            'recipient' : 'MOBILE_PHONE_1' \
+        }
+servers['s2'] = {
+            'server' : 'DOMAIN2.COM', \
+            'path' : '/',\
+            'port' : 80, \
+            'recipient' : 'MOBILE_PHONE_2' \
+        }
+
+
+def nexmo( to, message ):
+
+    # DEBUG
+    print "sends to ", to, "message: ", message  
+"""
     params = urllib.urlencode({
             'api_key' : '<NEXMO_API_KEY>',
             'api_secret' : '<NEXMO_API_SECRET>',
             'from' : '<FROM>',
-            'to' : '<TO>',
+            'to' : to,
             'text' : message
     })
 
@@ -18,7 +37,7 @@ def nexmo( message ):
     nex = httplib.HTTPSConnection("rest.nexmo.com", 443)
     nex.request("POST", "/sms/json", params, headers)
     r2 = nex.getresponse()
-
+"""
 def open_file():
     try:
         fh = open('status.dat', 'r+')
@@ -29,12 +48,22 @@ def open_file():
     return fh
     
 def status_on_file(fh):
+    status = {}
+
     for line in fh:
-        return int(line)
+        if line.strip():
+            l = line.split(' ')
+            status[l[0]] = int(l[1])
+
+    return status
     
-def current_status():
-    conn = httplib.HTTPSConnection("<SERVER_DOMAIN>", 443)
-    conn.request("GET", "/")
+def current_status(server, path, port):
+    if port == 443:
+        conn = httplib.HTTPSConnection(server, port)
+    if port == 80:
+        conn = httplib.HTTPConnection(server, port)
+    
+    conn.request("GET", path)
     r1 = conn.getresponse()
     
     return r1.status
@@ -42,17 +71,21 @@ def current_status():
 # start
    
 fl = open_file()
-
 previus_status = status_on_file(fl)
-current_status = current_status()
+c_status = ""
 
-if current_status != 200 and previus_status == 200:
-    nexmo('down')
+for s in servers:
+    current_status = current_status(servers[s]['server'], servers[s]['path'], servers[s]['port'])
+
+    if current_status != 200 and previus_status[s] == 200:
+        nexmo(servers[s]['recipient'], 'down')
     
-if current_status == 200 and previus_status != 200:
-    nexmo('up')
+    if current_status == 200 and previus_status[s] != 200:
+        nexmo(servers[s]['recipient'], 'up')
     
+    c_status+= s + ' ' + str(current_status) + "\n"
+
 fl.seek(0)    
-fl.write( str(current_status) )
+fl.write( c_status )
 fl.truncate()
 fl.close()
